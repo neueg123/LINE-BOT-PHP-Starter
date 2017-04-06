8e9f55b7d45b722a638b50d965090105bc6ced19
neueg123 <neueg_sutin@hotmail.com>
2017-04-07 02:45:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,22 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
-				{
-  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-  "type": "message",
-  "timestamp": 1462629479859,
-  "source": {
-    "type": "user",
-    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-  },
-  "message": {
-    "id": "325708",
-    "type": "text",
-    "text": "Hello, world"
-  }
-}
-				
+				'text' => $text		
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
