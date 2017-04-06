c85c000aad8fa1ffd1a538c5b59e4a32a59c6709
neueg123 <neueg_sutin@hotmail.com>
2017-04-07 02:31:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,11 +18,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
+				{
 				'type' => 'text',
-				'text' => $text ,
+				'text' => $text
+				},
+				{
 				'type' => 'text',
 				'text' => 'ไง'
-				
+				},
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
