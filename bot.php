4566ad99617c65daef88e3d7f33b72c4a3e02fa3
neueg123 <neueg_sutin@hotmail.com>
2019-02-02 16:50:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,6 +16,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['text'] = "สวัสดีครับเจ้านาย";
+  $arrPostData['messages'][0]['text'] = "มีอะไรให้รับใช้ครับ";
+  $arrPostData['messages'][0]['text'] = "ยินดีต้อนรับครับ";
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
